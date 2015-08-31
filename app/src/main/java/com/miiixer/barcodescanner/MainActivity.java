7138ddd59be84dbc2402197e5f39f63b961607a5
lightshdow <klop5692002@gmail.com>
2015-08-31 18:11:47 +0800

    google plays upload alpha

--- app/src/main/java/com/miiixer/barcodescanner/MainActivity.java
+++ app/src/main/java/com/miiixer/barcodescanner/MainActivity.java
@@ -2,8 +2,8 @@ package com.miiixer.barcodescanner;
 
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.Vibrator;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -13,16 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-import com.google.zxing.ResultPoint;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
-import com.journeyapps.barcodescanner.BarcodeView;
-import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
-import com.journeyapps.barcodescanner.ViewfinderView;
 
 import java.util.List;
 
@@ -85,9 +80,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(intentResult != null) {
+        if (intentResult != null) {
             String result = intentResult.getContents();
-            if(result != null) {
+            if (result != null) {
                 vibrator.vibrate(300);
                 showInfo.setText(intentResult.toString());
             }
